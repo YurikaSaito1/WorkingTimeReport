1fb8665e3665bd32ecc3adf2e5474209379f6857
YurikaSaito <y.saito.168@gmail.com>
2024-03-25 11:40:41 +0900

    企業選択ボタン追加

---
+++ companySelect.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/companySelect.css">
+        <title>作業時間報告</title>
+    </head>
+    <body>
+        <h1 class="subtitle">企業選択</h1>
+
+<?php
+// 接続
+$mysqli = new mysqli('localhost', 'root', '2856', 'my_app');
+$mysqli->set_charset('utf8');
+        
+//接続状況の確認
+if (mysqli_connect_errno()) {
+    echo "データベース接続失敗" . PHP_EOL;
+    echo "errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "error: " . mysqli_connect_error() . PHP_EOL;
+    exit();
+}
+
+$sql = "SELECT * FROM company_table";
+$stmt = $mysqli->prepare($sql);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$mysqli->close();
+
+while( $row_data = $result->fetch_array(MYSQLI_NUM) ) {
+    $company_code = $row_data[1];
+    $company_name = $row_data[2];
+?>
+
+        <form action="yearSelect.php" method="post">
+            <input type="hidden" name="company-code" value="<?= $company_code ?>">
+            <input class="submitButton" type="submit" value="<?= $company_name ?>">
+        </form>
+        
+<?php
+}
+?>
+        <div class="link">
+            <a href="index.html" id="topPage">トップページ</a>
+        </div>
+        <script src="js/companySelect.js"></script>
+    </body>
+</html>
